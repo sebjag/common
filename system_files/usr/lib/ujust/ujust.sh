7d39bef444da8c89c0ad0264440368f6000f65c6
Tulip Blossom <tulilirockz@outlook.com>
2025-11-23 20:41:10 -0300

    fix(ujust): remove distrobox/toolbox stuff (we dont need it :)
    Signed-off-by: Tulip Blossom <tulilirockz@outlook.com>

--- system_files/usr/lib/ujust/ujust.sh
+++ system_files/usr/lib/ujust/ujust.sh
@@ -6,7 +6,3 @@ source /usr/lib/ujust/libcolors.sh
 source /usr/lib/ujust/libformatting.sh
 # Import functionality for just
 source /usr/lib/ujust/libfunctions.sh
-# Import functionality related to distrobox
-source /usr/lib/ujust/libdistrobox.sh
-# Import functionality related to toolbox
-source /usr/lib/ujust/libtoolbox.sh
